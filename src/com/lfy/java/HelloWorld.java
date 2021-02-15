c735d751cdde0bf677b9e1d3e682877ac4793b2a
liufayang <liu.fayang@cesgroup.com.cn>
2021-02-15 19:39:09 +0800

    Initial commit

---
+++ src/com/lfy/java/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.lfy.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    /**
+     *
+     * @param args
+     */
+    private static final  String str = "sssss";
+    private static final Customer custome= new Customer();
+    public  static  void main(String[] args){
+        System.out.println("HelloWorld");
+        ArrayList<Object> objects = new ArrayList<>();
+        ArrayList arrayList = new ArrayList();
+        if (arrayList == null) {
+
+        }
+        if (arrayList != null) {
+
+        }
+        if (arrayList == null) {
+
+        }
+
+        objects.add(123);
+        objects.add(234);
+        for (Object object : objects) {
+            System.out.println(object);
+            System.out.println(object);
+        }
+        for (int i = 0; i < objects.size(); i++) {
+
+        }
+        for (int i = objects.size() - 1; i >= 0; i--) {
+
+        }
+        if (objects == null) {
+            
+        }
+
+
+    }
+
+}
